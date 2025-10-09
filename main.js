6f18a7566c036d9150828988120954bf3f5aec9c
RobynHockey <u22488244@tuks.co.za>
2025-10-09 12:15:05 +0200

    THREE

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as three from 'https://cdn.jsdelivr.net/npm/three@0.155/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.155/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.155/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.155/examples/jsm/loaders/GLTFLoader.js';
 
