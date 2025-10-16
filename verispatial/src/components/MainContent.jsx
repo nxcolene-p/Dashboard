1b256448b8e475daa54c86a3fec17dfb7841083b
RobynHockey <u22488244@tuks.co.za>
2025-10-16 13:48:13 +0200

    update site with vite

---
+++ verispatial/src/components/MainContent.jsx
@@ -0,0 +1,36 @@
+
+
+
+import React from 'react';
+import ThreeMap from './ThreeMap'; 
+
+export default function MainContent({ page }) {
+  let content;
+
+ 
+  switch (page) {
+    case 'Home':
+      content = (
+        <>
+          <h2>Home</h2>
+          <p>Welcome to VeriSpatial's dashboard. </p>
+        </>
+      );
+      break;
+    
+    case '3D Model':
+      content = (
+        <div style={{ flex: 1, width: '100%', height: '100%' }}>
+          <ThreeMap /> 
+        </div>
+      );
+      break;
+    
+  }
+
+  return (
+    <main style={{ flex: 1, padding: '1rem', backgroundColor: '#E8F5E9' }}>
+      {content}
+    </main>
+  );
+}
